6eadd8a3356c1eac158bf2e09c4d82d15ebd8a84
abenabbes <alibenabbes14@gmail.com>
2026-01-25 16:45:58 +0100

    Ajout de la structure de projet

--- src/test/java/com/example/demo/library/repository/AuthorRepositoryTest.java
+++ src/test/java/com/example/demo/library/repository/AuthorRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import com.example.demo.domain.entity.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -31,4 +32,34 @@ class AuthorRepositoryTest {
         assertThat(authors).hasSize(1);
         assertThat(authors.get(0).getBooks()).isNullOrEmpty();
     }
+
+    @Test
+    void should_return_authors_with_and_without_books() {
+        Author author1 = new Author("Author One");
+        Author author2 = new Author("Author Two");
+        authorRepository.save(author1);
+        authorRepository.save(author2);
+
+        bookRepository.save(new Book("Book One", author1));
+        bookRepository.save(new Book("Book Two", author1));
+
+        List<Author> authors = authorRepository.findAllWithBooks();
+
+        assertThat(authors).hasSize(2);
+
+        Author a1 = authors.stream()
+                .filter(a -> a.getName().equals("Author One"))
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(a1.getBooks()).hasSize(2);
+
+        Author a2 = authors.stream()
+                .filter(a -> a.getName().equals("Author Two"))
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(a2.getBooks()).isNullOrEmpty();
+    }
+
 }
